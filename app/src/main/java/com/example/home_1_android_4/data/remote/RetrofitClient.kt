3245c25035c13e4202a7e47123cd4acc0f7f622d
Abdulaziz865 <111993898+Abdulaziz865@users.noreply.github.com>
2023-02-04 16:31:26 +0300

    Home Work 1 Android 4

--- app/src/main/java/com/example/home_1_android_4/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/home_1_android_4/data/remote/RetrofitClient.kt
@@ -10,13 +10,13 @@ import java.util.concurrent.TimeUnit
 class RetrofitClient {
 
     private val okHttpClient = OkHttpClient()
-            .newBuilder()
-            .addInterceptor(provideLoggingInterceptor())
-            .callTimeout(30, TimeUnit.SECONDS)
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .build()
+        .newBuilder()
+        .addInterceptor(provideLoggingInterceptor())
+        .callTimeout(30, TimeUnit.SECONDS)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
 
     private val retrofitClient = Retrofit.Builder()
         .baseUrl("https://kitsu.io/api/edge/")
